76d98917dfc64209b3bf9c793cfb973d1b053e65
ParkAnthony13 <anth0nypark13@gmail.com>
2021-07-16 00:03:09 -0700

    consecutive  api call

--- src/Components/Results.jsx
+++ src/Components/Results.jsx
@@ -10,9 +10,11 @@ const Results = props => {
     const { formState } = props
     const [people, setPeople] = useState(0)
     const [error,setError] = useState(false)
+    const [errorHome,setErrorHome] = useState(false)
+    const [home,setHome] = useState("")
 
     useEffect(() => {
-        console.log("start")
+        console.log("Person")
         axios.get(`https://swapi.dev/api/people/${formState.num}`)
             .then(response => {
                 setPeople(response.data)
@@ -24,11 +26,26 @@ const Results = props => {
             })
     }, [])
 
+    useEffect(() => {
+        console.log("Home Finder")
+        axios.get(`${people.homeworld}`)
+            .then(response => {
+                setHome(response.data)
+                console.log(response.data)
+            })
+            .catch(err => {
+                console.log(`Encountered Error: ${err}`)
+                setErrorHome(true)
+            })
+    }, [people])
+
     return (
         <div>
             {error ? <div><img className="obi" src={'https://i.kym-cdn.com/entries/icons/original/000/018/682/obi-wan.jpg'} alt="obi"/><h1 style={{color:"red"}}>These are not the Droids you are looking for!</h1></div>
             : <div style={{display:"flex",flexDirection:"column",justifyContent:"space-evenly",alignItems:"center",width:"50%",margin:"auto"}}>
                 <h1>{people.name}</h1>
+                <h3>{people.homeworld}</h3>
+                {errorHome ? <h3>{home.name}</h3> : null}
                 <div style={{display:"flex",alignItems:"center"}}>
                     <h3 style={{margin:"6px"}}>Height:</h3>
                     <p>{people.height}</p>
